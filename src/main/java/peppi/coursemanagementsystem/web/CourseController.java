3bc8f56a475e0aed6bb6a25bcfe655e97f9db620
bhj665@myy.haaga-helia.fi <bhj665>
2025-11-24 22:02:11 +0200

    Js-tiedosto lisätty

--- src/main/java/peppi/coursemanagementsystem/web/CourseController.java
+++ src/main/java/peppi/coursemanagementsystem/web/CourseController.java
@@ -1,19 +1,22 @@
 package peppi.coursemanagementsystem.web;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import peppi.coursemanagementsystem.domain.Course;
-import java.ui.Model;
 @Controller
 public class CourseController {
-@GetMapping("/addcourse")
-public String getCourses(Model model) {
+@GetMapping("/makecourses")
+public String makeCourses(Model model) {
 List<Course> courses = new ArrayList<Course>();
 courses.add(new Course("Kurssi A", "Periodi 1", "5 op"));
 courses.add(new Course("Kurssi B", "Periodi 2", "5 op"));
 courses.add(new Course("Kurssi C", "Periodi 3", "5 op"));
-courses.add(new Course("Kurssi D", "Periodi 4", "5 op"));
+courses.add(new Course("Kurssi D", "Periodi 4", "1 op"));
 model.addAttribute("courses", courses);
+return "Lisääminen";
 }
 }
